a5d713e335d80ba3497ad324c82315e31113d253
Cameron Bradley <cameronbradley.git@gmail.com>
2012-10-20 19:46:19 +1100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvestorCaps::Application.config.secret_token = '879c6f094fce8a70c8dc6c604c78c70c36602c05654d0ed385d066431dff5997f77cc9ce7d225fcd1285b4facd94cf5dd27b561f1254844afd51da40003584e1'
